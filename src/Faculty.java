0862a3124f9f24ab735d05c1bd13c2454487ae53
Tsivka Galina <galinatsivka@gmail.come>
2020-11-17 00:09:07 +0200

    decision

--- src/Faculty.java
+++ src/Faculty.java
@@ -1,2 +1,22 @@
+import java.io.*;
+
 public class Faculty {
+
+    public static void savingGroupToDatabase(Group group) {
+        try (ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream(group.getNameGroup()))) {
+            write.writeObject(group);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Group getGroupDatabase(String nameGroup) {
+        Group group = new Group();
+        try (ObjectInputStream read = new ObjectInputStream(new FileInputStream(nameGroup))) {
+            group = (Group) read.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return group;
+    }
 }
